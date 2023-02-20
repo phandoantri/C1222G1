ed71d1c7f8954b7a631381bcabd7b364a0041195
phandoantri <pdtri1996@gmail.com>
2023-02-20 10:18:43 +0700

    first commit

--- module_2/src/ss3_array/bai_tap/DemSoLanXuatHien.java
+++ module_2/src/ss3_array/bai_tap/DemSoLanXuatHien.java
@@ -3,26 +3,30 @@ package ss3_array.bai_tap;
 import java.util.Scanner;
 
 public class DemSoLanXuatHien {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char[] array = new char[5];
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("nhap phan tu cho mang " + (i + 1));
-            array[i] = sc.next().charAt(0);
-        }
-        System.out.println("moi ban nhap tu can kiem tra ");
-        char chu = sc.next().charAt(0);
+    static String kyTu(char[] nums,char n) {
         int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (chu == array[i]) {
+        for (int i = 0; i < nums.length; i++) {
+            if (n == nums[i]) {
                 count++;
             }
         }
         if (count == 0) {
-            System.out.println("khong co ky tu can tim trong mang ");
+            return "khong co trung phan tu trong mang";
         } else {
-            System.out.println("co" + count + chu + "trong mang");
+            return "co" + " " + count + " " + n + " " + "trong mang";
+        }
+    }
+    public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            char[] array = new char[5];
+            for (int i = 0; i < array.length; i++) {
+                System.out.println("nhap phan tu cho mang " + (i + 1));
+                array[i] = sc.next().charAt(0);
+            }
+            System.out.println("moi ban nhap tu can kiem tra ");
+            char chu = sc.next().charAt(0);
+        System.out.println(kyTu(array,chu));
+
         }
 
-    }
 }
--- module_2/src/ss3_array/bai_tap/TongOCotJ.java
+++ module_2/src/ss3_array/bai_tap/TongOCotJ.java
@@ -4,25 +4,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TongOCotJ {
+    static int tinhTong1Cot(int[][] nums, int n) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = n; j <= n; j++) {
+                sum += nums[i][n];
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int[][] array=new int[2][3];
-        for (int i=0;i<array.length;i++) {
+        Scanner sc = new Scanner(System.in);
+        int[][] array = new int[2][3];
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.println("nhap phan tu cho mang " + (i + 1) + " " + (j + 1));
                 array[i][j] = sc.nextInt();
             }
         }
-            System.out.println(Arrays.deepToString(array));
-            System.out.println("moi ban nhap vi tri muon tinh tong");
-            int k= sc.nextInt();
-        int sum=0;
-            for (int i=0;i<array.length;i++){
-                for (int j=k;j<=k;j++){
-                    sum=sum+array[i][k];
-                }
-            }
-        System.out.println(sum);
-        }
+        System.out.println("mang ban can tinh la " + Arrays.deepToString(array));
+        System.out.println("nhap cot ban can tinh tong");
+        int k = sc.nextInt();
+        System.out.println("tong o cot" + " " + k + " " + "la" + " " + tinhTong1Cot(array, k));
     }
+}
 
