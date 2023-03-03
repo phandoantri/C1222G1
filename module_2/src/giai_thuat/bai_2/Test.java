1e7fed938ecaa9fd8279aa03ef846263420b5030
phandoantri <pdtri1996@gmail.com>
2023-03-03 10:27:25 +0700

    first commit

---
+++ module_2/src/giai_thuat/bai_2/Test.java
@@ -0,0 +1,12 @@
+package giai_thuat.bai_2;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr =new int[5];
+        int[] arr1=new int[5];
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+
+    }
+}
