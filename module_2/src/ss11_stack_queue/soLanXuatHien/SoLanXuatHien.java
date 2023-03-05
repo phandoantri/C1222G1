24310dbc502df5c9f69a85259037ef4f498b2b2c
phandoantri <pdtri1996@gmail.com>
2023-03-05 10:41:56 +0700

    first commit

--- module_2/src/ss11_stack_queue/soLanXuatHien/SoLanXuatHien.java
+++ module_2/src/ss11_stack_queue/soLanXuatHien/SoLanXuatHien.java
@@ -1,15 +1,28 @@
 package ss11_stack_queue.soLanXuatHien;
 
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class SoLanXuatHien {
     public static void main(String[] args) {
-       TreeMap<String,String> map=new TreeMap<String,String>();
-       map.put("j","japan");
-       map.put("a","agola");
-       map.put("i","italia");
+        Map<Character, Integer> map = new TreeMap<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap chuoi muon kiem tra ");
+        String str = sc.nextLine();
+        Character key;
+        for (int i = 0; i < str.length(); i++) {
+            key=str.toUpperCase().charAt(i);
+            int count=0;
+            if (map.containsKey(key)){
+                count=map.get(key);
+                map.put(key,count+1);
+            }else {
+                map.put(key,1);
+            }
+        }
 
-
-
+        System.out.println(map);
     }
 }
