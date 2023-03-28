121bd5f140860c91e336f248c81180052a3492de
phandoantri <pdtri1996@gmail.com>
2023-03-28 20:26:10 +0700

    first commit

--- module_3/ss3_task1/ss3_task1.sql
+++ module_3/ss3_task1/ss3_task1.sql
@@ -1,2 +1,77 @@
+drop database if exists ss3_task1;
 create database ss3_task1;
+use ss3_task1;
+create table classroom (
+    class_id int not null auto_increment primary key,
+    class_name varchar(60) not null,
+    start_day datetime not null,
+    status bit
+);
+create table student(
+student_id int not null auto_increment primary key,
+    student_name varchar(30) not null,
+    address     varchar(50),
+    phone       varchar(20),
+    status      bit,
+    class_id  int not null,
+    foreign key (class_id) references classroom (class_id)
+);
+create table student_subject(
+sub_id int not null auto_increment primary key,
+    sub_name varchar(30) not null,
+    credit  tinyint  not null default 1 check ( credit >= 1 ),
+    status  bit   default 1
+);
+create table mark(
+	mark_id    int not null auto_increment primary key,
+    sub_id     int not null,
+    student_id int not null,
+    mark      float   default 0 check ( mark between 0 and 100),
+    exam_times tinyint default 1,
+    unique (sub_id, student_id),
+    foreign key (sub_id) references student_subject (sub_id),
+    foreign key (student_id) references student (student_id)
+);
+insert into classroom (class_id, class_name, start_day, status)
+values (1, 'A1', '2008-12-20', 1);
+insert into classroom (class_id, class_name, start_day, status)
+values (2, 'A2', '2008-12-22', 1);
+insert into classroom (class_id, class_name, start_day, status)
+values (3, 'B3', current_date, 0);
+insert into student (student_name, address, phone, status, class_id)
+values ('Hung', 'Ha Noi', '0912113113', 1, 1);
+insert into student (student_name, address, status, class_id)
+values ('Hoa', 'Hai phong', 1, 1);
+insert into student (student_name, address, phone, status, class_id)
+values ('Manh', 'HCM', '0123123123', 0, 2);
+insert into student_subject (sub_id,sub_name,credit,status)
+values (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+insert into mark (sub_id, student_id, mark, exam_times)
+values (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+select *from student 
+where student_name like 'h%';
+select*from classroom
+where month(start_day) ='12';
+select*from student_subject
+where credit>=3 and credit<=5;
+set sql_safe_updates=0;
+update student set class_id=2 where student_name="hung";
+set sql_safe_updates=1;
+
+select*from student;
+
+
+select student.student_name,student_subject.sub_name,mark.mark from student
+inner join student_subject
+on student.student_id=student_subject.sub_id
+inner join mark
+on student_subject.sub_id=mark.mark_id
+order by mark desc;
+
+
 
---
+++ module_3/ss3_task2/ss3_task2.sql
@@ -0,0 +1,79 @@
+drop database if exists ss3_task2;
+create database ss3_task2;
+use ss3_task2;
+create table customer(
+id int primary key,
+customer_name varchar(45),
+age int
+);
+create table order_product(
+order_id int primary key,
+customer_id int,
+order_date date,
+order_total_prime double,
+foreign key(customer_id) references customer(id)
+);
+create table product(
+product_id int primary key,
+product_name varchar(45),
+product_prime double
+);
+create table order_detail(
+order_id int,
+product_id int,
+order_quality varchar(45),
+primary key(order_id,product_id),
+foreign key(order_id) references order_product(order_id),
+foreign key(product_id) references product(product_id)
+);
+insert into customer (id,customer_name,age)
+values (1,'minh quan',10);
+insert into customer (id,customer_name,age)
+values (2,'ngoc oanh',20);
+insert into customer (id,customer_name,age)
+values (3,'hong ha',50);
+insert into order_product (order_id,customer_id,order_date,order_total_prime)
+values (1,1,'2006-03-21',null);
+insert into order_product (order_id,customer_id,order_date,order_total_prime)
+values (2,2,'2006-03-23',null);
+insert into order_product (order_id,customer_id,order_date,order_total_prime)
+values (3,1,'2006-03-16',null);
+insert into product (product_id,product_name,product_prime)
+values (1,'may giat',3);
+insert into product (product_id,product_name,product_prime)
+values (2,'tu lanh',5);
+insert into product (product_id,product_name,product_prime)
+values (3,'dieu hoa',7);
+insert into product (product_id,product_name,product_prime)
+values (4,'quat',1);
+insert into product (product_id,product_name,product_prime)
+values (5,'bep dien',2);
+insert into order_detail (order_id,product_id,order_quality)
+values (1,1,3);
+insert into order_detail (order_id,product_id,order_quality)
+values (1,3,7);
+insert into order_detail (order_id,product_id,order_quality)
+values (1,4,2);
+insert into order_detail (order_id,product_id,order_quality)
+values (2,1,1);
+insert into order_detail (order_id,product_id,order_quality)
+values (3,1,8);
+insert into order_detail (order_id,product_id,order_quality)
+values (2,5,4);
+insert into order_detail (order_id,product_id,order_quality)
+values (2,3,3);
+select order_id,order_date,order_total_prime from order_product;
+select customer.customer_name,product.product_name from customer
+inner join order_product on customer.id=order_product.customer_id
+inner join product on order_product.customer_id=product.product_id;
+select customer.customer_name from customer
+where id not in (1,2);
+select order_detail.order_id,order_product.order_date,product.product_prime from product
+inner join order_detail on product.product_id=order_detail.product_id
+inner join order_product on order_detail.order_id=order_product.order_id;
+
+
+
+
+
+
