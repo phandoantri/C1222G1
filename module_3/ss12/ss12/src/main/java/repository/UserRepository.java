ae0c8cab022f3aa7fe352edc95e0977d37d032cd
phandoantri <pdtri1996@gmail.com>
2023-04-17 15:21:25 +0700

    ss13

--- module_3/ss12/ss12/src/main/java/repository/UserRepository.java
+++ module_3/ss12/ss12/src/main/java/repository/UserRepository.java
@@ -3,6 +3,7 @@ package repository;
 import com.mysql.cj.x.protobuf.MysqlxPrepare;
 import model.User;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +18,7 @@ public class UserRepository implements IUserRepository {
     private static final String DELETE_USERS_SQL = "delete from user where id = ?;";
     private static final String UPDATE_USERS_SQL = "update user set name = ?,email= ?, country =? where id = ?;";
     private static final String SELECT_USER_BY_ID = "select id,name,email,country from user where id =?";
+
     @Override
     public List<User> getOn() {
         List<User> userList = new ArrayList<>();
@@ -73,16 +75,16 @@ public class UserRepository implements IUserRepository {
         try {
             PreparedStatement preparedStatement = BaseRepository.getConnection().
                     prepareStatement(SELECT_USER_BY_COUNTRYNAME);
-            preparedStatement.setString(1,country );
+            preparedStatement.setString(1, country);
             ResultSet resultSet = preparedStatement.executeQuery();
             User user;
             while (resultSet.next()) {
-               user=new User();
-               user.setId(resultSet.getInt("id"));
-               user.setName(resultSet.getString("name"));
-               user.setEmail(resultSet.getString("email"));
-               user.setCountry(resultSet.getString("country"));
-               userList.add(user);
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setEmail(resultSet.getString("email"));
+                user.setCountry(resultSet.getString("country"));
+                userList.add(user);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -96,11 +98,11 @@ public class UserRepository implements IUserRepository {
         try {
             PreparedStatement preparedStatement = BaseRepository.getConnection().
                     prepareStatement(SELECT_USER_BY_ID);
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             User user = null;
             if (resultSet.next()) {
-                user=new User();
+                user = new User();
                 user.setId(resultSet.getInt("id"));
                 user.setName(resultSet.getString("name"));
                 user.setEmail(resultSet.getString("email"));
@@ -123,11 +125,24 @@ public class UserRepository implements IUserRepository {
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getEmail());
             preparedStatement.setString(3, user.getCountry());
-            preparedStatement.setInt(4,id);
-           preparedStatement.executeUpdate();
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    public void addUserTransaction() {
+        Connection connection = null;
+        try {
+            connection = BaseRepository.getConnection();
+            connection.setAutoCommit(false);
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id,name, email, country) values (10,'nguyenhuu', 'nguyenhuu@gmail.com',10)");
+            preparedStatement.executeUpdate();
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- module_3/ss12/ss12/src/main/webapp/view/list.jsp
+++ module_3/ss12/ss12/src/main/webapp/view/list.jsp
@@ -10,6 +10,17 @@
 <html>
 <head>
     <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
 </head>
 <body>
 <button><a href="/user?action=create">create</a></button>
@@ -36,8 +47,36 @@
             <td>${user.getEmail()}</td>
             <td>${user.getCountry()}</td>
             <td>
-                <button><a href="/user?action=delete&id=${user.getId()}">delete</a></button>
+                    <%--                <button><a href="/user?action=delete&id=${user.getId()}">delete</a></button>--%>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal${user.getId()}">
+                    delete
+                </button>
             </td>
+                <!-- Modal -->
+                <form action="/user" method="get">
+                    <div class="modal fade" id="exampleModal${user.getId()}" tabindex="-1" role="dialog"
+                         aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Xoa hoc sinh</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <input type="hidden" name="action" value="delete">
+                                <input type="hidden" name="id" value="${user.id}">
+                                <div class="modal-body">
+                                    ban co muon xoa ${user.getName()}
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </form>
             <td>
                 <button><a href="/user?action=update&id=${user.getId()}">update</a></button>
             </td>
